b4b57aa3d12042e13d69c3ea755f7c4bdaeda88c
samuel kyama <samuelkyama102@gmail.com>
2025-08-26 12:44:24 +0300

    refactor: Remove DisbursePayments component as it is no longer needed

--- app/disburse-payments.jsx
+++
@@ -1,7 +0,0 @@
-import DisbursePaymentsComponent from "@/components/DisbursePayments/DisbursePayments";
-
-export default function DisbursePayments() {
-  return (
-    <DisbursePaymentsComponent/>
-  );
-}
